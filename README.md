2f31c1ebc8a584037a60791f23288656ffdc09de
Erdem Kara <erdem8kara@gmail.com>
2024-05-15 15:05:11 +0200

    add comments and images

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
-# Public Safety Group
+# Public Safety Group | Board Game with Arduino
 
-## Hacking Innovation Bias SS24
+## Hacking Innovation Bias | SoSe 2024
 
-### How to use?
+### How to use Arduino?
 
-* git clone https://github.com/errdemk/public-safety.git
+* Clone the project: `git clone https://github.com/errdemk/public-safety.git`
 * Open the project in Arduino IDE
 * Plug in the Arduino board
 * Select **Board** and **Port** in the **Tools** section
 * **Verify** and **Upload** the code
 * Try **Action Field**, **Dice** and **Card** buttons
+* **Arduino artifact:**
 
-[TODO] Improve README
+<img src="images/arduino1.jpg" width=40%>
+<img src="images/arduino2.jpg" width=26%>
+
+### Other components
+
+* **Game board:**
+
+<img src="images/board.jpg" width=40%>
+
+* **Cards:**
+
+<img src="images/cards.jpg" width=35%>
+
+* **A dice and the figures:**
+
+<img src="images/dicefig.jpg" width=35%>
--- public-safety.ino
+++ public-safety.ino
@@ -3,20 +3,28 @@
 
 rgb_lcd lcd;
 
+// The background color of the LCD screen can be adjusted by changing "colorR", "colorG" and "colorB"
 const int colorR = 0;
 const int colorG = 0;
 const int colorB = 255;
 
+// New action fields can be added.
+// If new action fields are added, it is important to change the random value range in "getField" function
+//  e.g. if "Supermarket" is added to "afields", set "lcd.print("Field:" + afields[random(0, 7)]);"
 String afields[] = {"University", "Park", "Job", "Sport", "Party", "Transport"};
 
+// "pin" variables specify the number of digital pins of the Arduino board
+// The cables of the LED and the given buttons should be connected to these numbers
 const int ledPin = 13;
 const int fieldButtonPin = 1;
 const int diceButtonPin = 3;
 const int cardButtonPin = 5;
+
 int fieldButtonState = 0;
 int diceButtonState = 0;
 int cardButtonState = 0;
 
+// Clears the given LCD line
 void clearLCDLine(int line)
 {
     for(int n = 0; n < 16; n++)
@@ -36,7 +44,7 @@ void setup()
     pinMode(diceButtonPin, INPUT_PULLUP);
     pinMode(cardButtonPin, INPUT_PULLUP);
 
-    // set up the LCD's number of columns and rows:
+    // set up the LCD's number of columns and rows
     lcd.begin(16, 2);
     lcd.setRGB(colorR, colorG, colorB);
 }
@@ -65,9 +73,10 @@ void getCard()
     clearLCDLine(1);
     // Print result of selected card
     randomSeed(analogRead(0));
-    lcd.print("Card: " + String(random(1, 4)));
+    lcd.print("Card: " + String(random(1, 5)));
 }
 
+// This function runs in a loop to detect button presses
 void loop() 
 {
     fieldButtonState = digitalRead(fieldButtonPin);
